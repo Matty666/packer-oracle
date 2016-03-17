873d8e8e10a64425d5c8ca66ebdde5741acdf35c
Edwin Biemond <biemond@gmail.com>
2016-03-17 11:25:15 +0100

    fixes for RHEL7 with eth0

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-wget -O/etc/pki/tls/certs/ca-bundle.crt http://curl.haxx.se/ca/cacert.pem
-groupadd vagrant -g 999
+# add official mozilla certs
+wget --no-check-certificate -O/etc/pki/tls/certs/ca-bundle.crt http://curl.haxx.se/ca/cacert.pem
+
+groupadd vagrant -g 1999
 useradd vagrant -g vagrant -G wheel -u 900
 echo "vagrant" | passwd --stdin vagrant
 echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
@@ -10,7 +12,12 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 mkdir /home/vagrant/.ssh
 wget --no-check-certificate -O authorized_keys 'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub'
 mv authorized_keys /home/vagrant/.ssh
-chown -R vagrant /home/vagrant/.ssh
+chown -R vagrant:vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
+
+#fix selinux
+restorecon -R -v /home/vagrant/.ssh
+
+echo 'UseDNS no' >> /etc/ssh/sshd_config
 
 yum install -y net-tools bind-utils nfs-common portmap rpcbind libgssglue nfs-utils keyutils libevent nfs-utils-lib
